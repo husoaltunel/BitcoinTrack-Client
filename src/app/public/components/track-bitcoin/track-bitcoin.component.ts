586e044c83701c21d4b14d98c05bc91823ca1b18
Hüseyin ALTUNEL <65026269+husoaltunel@users.noreply.github.com>
2022-01-18 02:49:57 +0300

    added timer

--- src/app/public/components/track-bitcoin/track-bitcoin.component.ts
+++ src/app/public/components/track-bitcoin/track-bitcoin.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { BitconDetail } from 'src/app/shared/models/bitcoin-detail.model';
 import { BitcoinDetailService } from 'src/app/shared/services/bitcoin-detail.service';
 
@@ -7,38 +9,52 @@ import { BitcoinDetailService } from 'src/app/shared/services/bitcoin-detail.ser
   templateUrl: './track-bitcoin.component.html',
   styleUrls: ['./track-bitcoin.component.css']
 })
-export class TrackBitcoinComponent implements OnInit {
+export class TrackBitcoinComponent implements OnInit, OnDestroy {
 
   data: any;
-  minValue : string;
-  maxValue : string;
-  constructor(private bitcoinDetailService : BitcoinDetailService) {
-  
-    this.data = [{time: new Date(2022, 0, 17, 1),open: 40000.07,}]
+  minValue: string;
+  maxValue: string;
+  timerSubscription: Subscription;
+  taskDelay : number;
+  constructor(private bitcoinDetailService: BitcoinDetailService) {
     this.getBitcoinDetails();
+    this.taskDelay = 30000;
   }
   ngOnInit(): void {
+    this.timerSubscription = timer(0, this.taskDelay).pipe(
+      map(() => {
+        this.getBitcoinDetails();
+      })
+    ).subscribe();
   }
-  
-  getBitcoinDetails(){
-    this.bitcoinDetailService.getAll().subscribe((response : BitconDetail[]) => {
+  ngOnDestroy(): void {
+    this.timerSubscription.unsubscribe();
+  }
+
+  getBitcoinDetails() {
+    this.bitcoinDetailService.getAll().subscribe((response: BitconDetail[]) => {
       this.setData(response);
     })
   }
 
-  setData(response : BitconDetail[]){
+  setData(response: BitconDetail[]) {
     this.setAxes(response);
+    this.initializeFirstChartData();
 
-    response.forEach(bitcoinDetail =>{
-      this.data.push({time : new Date(bitcoinDetail.date),open :`${bitcoinDetail.price.toString().slice(0,5)}.${bitcoinDetail.price.toString().slice(5)}`})
-    } )
+    response.forEach(bitcoinDetail => {
+      this.data.push({ time: new Date(bitcoinDetail.date), open: `${bitcoinDetail.price.toString().slice(0, 5)}.${bitcoinDetail.price.toString().slice(5)}` })
+    })
     this.data.title = "Bitcoin"
-  
   }
- setAxes(response : BitconDetail[]){
-  let prices = response.map(bitcoinDetail => bitcoinDetail.price)
-  this.minValue = Math.min(...prices).toString().slice(0,5);
-  this.maxValue = (Math.max(...prices)+50000).toString().slice(0,5);
- }
+
+  setAxes(response: BitconDetail[]) {
+    let prices = response.map(bitcoinDetail => bitcoinDetail.price)
+    this.minValue = Math.min(...prices).toString().slice(0, 5);
+    this.maxValue = (Math.max(...prices) + 50000).toString().slice(0, 5);
+  }
+
+  initializeFirstChartData() {
+    this.data = [{ time: new Date(2022, 0, 17, 1), open: 40000.07, }]
+  }
 
 }
